c86ac1e685715602b1a7133b60926318d3affe19
welly-im <wellyimanuel86@gmail.com>
2022-11-22 11:54:45 +0700

    prod

--- src/data/api.js
+++ src/data/api.js
@@ -1,9 +1,9 @@
 import { atom } from "jotai";
 
-export const apiPost = atom("http://192.168.0.105/server-ta/");
-export const apiGet = atom("http://192.168.0.105/server-ta/?smartfarming=");
+export const apiPost1 = atom("http://192.168.0.105/server-ta/");
+export const apiGet1 = atom("http://192.168.0.105/server-ta/?smartfarming=");
 
-export const apiPost1 = atom("https://sismartfarming.000webhostapp.com/");
-export const apiGet1 = atom(
+export const apiPost = atom("https://sismartfarming.000webhostapp.com/");
+export const apiGet = atom(
   "https://sismartfarming.000webhostapp.com/?smartfarming="
 );
